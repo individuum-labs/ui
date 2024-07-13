44429624626e263cdd903c46af59190a0e1fe85e
HuixuLiu <huixuliu@qq.com>
2024-07-13 23:15:06 +0200

    Initial commit

---
+++ src/app/marketplace/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Marketplace } from "@/components/component/Marketplace";
+
+export default function Home() {
+  
+  return (
+    <>
+      <Marketplace />
+    </>
+  );
+}
