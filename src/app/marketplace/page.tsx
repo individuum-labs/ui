55b84926dd2b87deb972d9b4e80b6b17d7830028
HuixuLiu <huixuliu@qq.com>
2024-07-14 02:59:15 +0200

    Style changing and coreectness

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Marketplace } from "@/components/component/Marketplace";
+import { Marketplace } from "@/components/component/marketplace";
 
 export default function Home() {
   
